885b62d9deb2f7fc2e8be8162480655b741636d2
ligerlac <lino.oscar.gerlach@cern.ch>
2023-02-17 11:35:23 +0100

    remove debug messages

--- src/dunenpc.cpp
+++ src/dunenpc.cpp
@@ -8,13 +8,11 @@ json Client::getUrlDict(long long run_number) {
 
 json Client::insertPayload(std::string pl_type, std::string file_url,
                            long long run_number_start) {
-    std::cout << "dunenpc::Client::insertPayload()" << std::endl;
     return nopayloadclient::Client::insertPayload(pl_type, file_url, run_number_start, 0);
 }
 
 json Client::insertPayload(std::string pl_type, std::string file_url,
                            long long run_number_start, long long run_number_end) {
-    std::cout << "dunenpc::Client::insertPayload()" << std::endl;
     return nopayloadclient::Client::insertPayload(pl_type, file_url, run_number_start, 0, run_number_end, 0);
 }
 
