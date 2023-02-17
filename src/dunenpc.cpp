de00935bcbdd83eb3cae016b9901e23611e28873
ligerlac <lino.oscar.gerlach@cern.ch>
2023-02-17 11:15:04 +0100

    use major- instead of minor-iov for run number (in case subrun precision will be needed)

--- src/dunenpc.cpp
+++ src/dunenpc.cpp
@@ -3,19 +3,19 @@
 namespace dunenpc {
 
 json Client::getUrlDict(long long run_number) {
-    return nopayloadclient::Client::getUrlDict(0, run_number);
+    return nopayloadclient::Client::getUrlDict(run_number, 0);
 }
 
 json Client::insertPayload(std::string pl_type, std::string file_url,
                            long long run_number_start) {
     std::cout << "dunenpc::Client::insertPayload()" << std::endl;
-    return nopayloadclient::Client::insertPayload(pl_type, file_url, 0, run_number_start);
+    return nopayloadclient::Client::insertPayload(pl_type, file_url, run_number_start, 0);
 }
 
 json Client::insertPayload(std::string pl_type, std::string file_url,
                            long long run_number_start, long long run_number_end) {
     std::cout << "dunenpc::Client::insertPayload()" << std::endl;
-    return nopayloadclient::Client::insertPayload(pl_type, file_url, 0, run_number_start, 0, run_number_end);
+    return nopayloadclient::Client::insertPayload(pl_type, file_url, run_number_start, 0, run_number_end, 0);
 }
 
 }
